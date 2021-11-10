775f763d7b9890a97e11547f6d2e1fb855c568c6
Juraj Trappl <juro.trappl@gmail.com>
2021-11-11 00:03:52 +0100

    add type to main func

--- 03-largest-prime-factor/solution.hs
+++ 03-largest-prime-factor/solution.hs
@@ -4,5 +4,6 @@ value = 600851475143
 sieve :: Integer -> [Integer]
 sieve n = [x | x <- [2..n], and [x `mod` y /= 0 | y <- [2..floor(sqrt(fromIntegral x))]]]
 
+main :: IO ()
 main = do
     print $ filter (\ n -> value `mod` n == 0) $ sieve value
