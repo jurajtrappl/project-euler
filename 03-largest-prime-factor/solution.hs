af1514a78a33237154b25aed954d4b1d484f890f
Juraj Trappl <juro.trappl@gmail.com>
2021-11-10 23:55:39 +0100

    solve 3. problem

---
+++ 03-largest-prime-factor/solution.hs
@@ -0,0 +1,8 @@
+value :: Integer 
+value = 600851475143
+
+sieve :: Integer -> [Integer]
+sieve n = [x | x <- [2..n], and [x `mod` y /= 0 | y <- [2..floor(sqrt(fromIntegral x))]]]
+
+main = do
+    print $ filter (\ n -> value `mod` n == 0) $ sieve value
