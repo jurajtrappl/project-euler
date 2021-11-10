9c805019d0dafd8d8d907c6925cd65d2bd92b460
Juraj Trappl <juro.trappl@gmail.com>
2021-11-11 00:54:50 +0100

    refactored solution using lcm func

--- 05-smallest-multiple/solution.hs
+++ 05-smallest-multiple/solution.hs
@@ -1,6 +1,3 @@
-isEvenlyDivisibleByNums :: Integral a => a -> Bool
-isEvenlyDivisibleByNums n = all (\ x -> n `mod` x == 0) [2..20]
-
 main :: IO ()
 main = do
-    print $ take 1 $ filter isEvenlyDivisibleByNums [100000000..]
+    print $ foldl lcm 1 [1..20]
