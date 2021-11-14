67eebc1f4ceaa7d92733ada0c30f13999a1504a5
Juraj Trappl <juro.trappl@gmail.com>
2021-11-14 23:00:00 +0100

    solution to 30. problem

---
+++ 30-digit-fifth-powers/solution.hs
@@ -0,0 +1,13 @@
+import qualified Data.Map as M
+
+powers :: M.Map Int Int
+powers = M.fromList [(0, 0), (1, 1), (2, 32), (3, 243), (4, 1024), (5, 3125), (6, 7776), (7, 16807), (8, 32768), (9, 59049)]
+
+digitPowerSum :: Int -> Int
+digitPowerSum n
+    | n /= 0 = powers M.! mod n 10 + digitPowerSum (div n 10)
+    | otherwise = 0
+
+main :: IO ()
+main = do
+    print $ filter (\ n -> digitPowerSum n == n) [2..1000000]
