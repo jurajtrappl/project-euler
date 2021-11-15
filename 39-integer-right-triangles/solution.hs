e7072dbef4b4ec01e4d7bd6a7281e016b797e4b5
Juraj Trappl <juro.trappl@gmail.com>
2021-11-15 22:20:03 +0100

    solution to 39. problem

---
+++ 39-integer-right-triangles/solution.hs
@@ -0,0 +1,15 @@
+import Data.List (group , sort)
+
+summedPythTriplets :: [Int]
+summedPythTriplets = [a + b + c | (a, b, c) <- [(x, y, z) | x <- [1..1000],
+                                                            y <- [1..1000],
+                                                            z <- [1..1000],
+                                                            x < y && y < z],
+                                                            a ^ 2 + b ^ 2 == c ^ 2]
+
+main :: IO ()
+main = do
+    print $ map ( \ xs -> (head xs, length xs))
+            $ group
+            $ filter (<= 1000)
+            $ sort summedPythTriplets
